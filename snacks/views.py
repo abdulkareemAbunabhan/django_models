c9dbaca791365d8adcb332bbe005ca1ece0632ca
abdulkareemAbunabhan <zman17881@gmail.com>
2023-06-15 05:46:38 +0000

    almost there

--- snacks/views.py
+++ snacks/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Snack
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -11,5 +12,7 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = 'about.html'
 
-class SnacksPageView(TemplateView):
+class SnacksPageView(ListView):
     template_name= 'snacks.html'
+    model = Snack
+    context_object_name = "Snacks"
